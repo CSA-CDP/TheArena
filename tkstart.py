79c6cc25ddba6f97dc4dbdbd8ee885099a0950b9
CSA-CDP <0019@chaparralstaracademy.com>
2017-03-07 07:53:48 -0600

    Delete tkstart.py

--- tkstart.py
+++
@@ -1,9 +0,0 @@
-#Collin Patterson
-#8/31/2016
-#Mr. Davis
-#Advance Computer Programming
-from tkinter import *
-from tkinter import ttk
-root=Tk()
-ttk.button(root, text="Hello World").grid()
-root.mainloop()
